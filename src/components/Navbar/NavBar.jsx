09ea03d9138a59caa622efd7046d92230634a92b
YRober <yomirobera@gmail.com>
2023-06-08 11:38:05 +0200

    fixed the navigation and profile

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -2,20 +2,22 @@ import keycloak from "../keycloak/keycloak";
 import { NavLink, useNavigate } from "react-router-dom";
 import { addUsers, getUser } from "../../api/user";
 
+import './NavBar.css';
+
 const NavBar = () => {
   const navigate = useNavigate();
 
   if (keycloak.authenticated) {
-    console.log(keycloak.tokenParsed.sub)
+    console.log(keycloak.tokenParsed.sub);
     getUser(keycloak.tokenParsed.sub)
       .then(result => {
-        console.log(result)
+        console.log(result);
         if (!result) {
-          console.log("POST")
+          console.log("POST");
           addUsers();
         } else {
           // do nothing
-          console.log("User exists, NO POST")
+          console.log("User exists, NO POST");
         }
       })
       .catch(error => {
@@ -24,58 +26,45 @@ const NavBar = () => {
   }
 
   return (
-    <div className="registrer">
+    <div className="navbar">
       {!keycloak.authenticated && (
-        <>
-          <button
-            onClick={() => keycloak.login()}
-          >
+        <div className="navbar__item navbar__item--right">
+          <button className="navbar__button" onClick={() => keycloak.login()}>
             Logg inn
           </button>
-
-          <button
-            onClick={() => keycloak.register()}
-          >
+          <button className="navbar__button" onClick={() => keycloak.register()}>
             Ny bruker
           </button>
-        </>
+        </div>
       )}
 
       {keycloak.authenticated && (
         <div className="navbar__item">
-          <NavLink className="tekst" to="/">
-            <button>
-              Stillingsannonser
-            </button>
+          <NavLink className="navbar__link" activeClassName="active" to="/">
+            Stillingsannonser
           </NavLink>
         </div>
       )}
 
       {keycloak.authenticated && (
         <div className="navbar__item">
-          <NavLink className="tekst" to="/leggetiljobb">
-            <button>
-              Legg til jobb
-            </button>
-
+          <NavLink className="navbar__link" activeClassName="active" to="/leggetiljobb">
+            Legg til jobb
           </NavLink>
         </div>
       )}
 
       {keycloak.authenticated && (
         <div className="navbar__item">
-          <NavLink className="tekst" to="/Profil">
-            <button>
-              Profil
-            </button>
-
+          <NavLink className="navbar__link" activeClassName="active" to="/Profil">
+            Profil
           </NavLink>
         </div>
       )}
 
       {keycloak.authenticated && (
         <button
-          className="navbar__item logout-button"
+          className="navbar__item navbar__button logout-button"
           onClick={() => {
             keycloak.logout();
             navigate("/");
@@ -85,6 +74,7 @@ const NavBar = () => {
         </button>
       )}
     </div>
-  )
-}
+  );
+};
+
 export default NavBar;
--- src/components/profil/Profil.jsx
+++ src/components/profil/Profil.jsx
@@ -1,10 +1,112 @@
-import withAuth from "../../hoc/withAuth"
+import withAuth from "../../hoc/withAuth";
+import { useEffect, useState, useCallback } from 'react';
+import keycloak from "../keycloak/keycloak";
+import { apiUrl } from "../../api/user";
+
+import './Profil.css';
 
 const Profil = () => {
+    const [user, setUser] = useState(null);
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+
+    const fetchUser = useCallback(async () => {
+        try {
+            // Update token and get user ID
+            await keycloak.updateToken(5);
+            const userId = keycloak.tokenParsed.sub;
+
+            // Fetch user data with token in Authorization header 
+            const response = await fetch(`${apiUrl}/${userId}`);
+
+            if (!response.ok) {
+                throw new Error(`Failed to fetch user. Status code: ${response.status}`);
+            }
+
+            const fetchedUser = await response.json();
+            setUser(fetchedUser);
+            setFirstName(fetchedUser.firstName);
+            setLastName(fetchedUser.lastName);
+            setEmail(fetchedUser.email);
+        } catch (error) {
+            // Handle error
+        }
+    }, []);
+
+    useEffect(() => {
+        fetchUser();
+    }, [fetchUser]);
+
+    const handleInputChange = (event) => {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+
+        if (name === 'firstName') {
+            setFirstName(value);
+        } else if (name === 'lastName') {
+            setLastName(value);
+        } else if (name === 'email') {
+            setEmail(value);
+        }
+    };
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const response = await fetch(`${apiUrl}/${keycloak.tokenParsed.sub}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    id: keycloak.tokenParsed.sub,
+                    firstName: firstName,
+                    lastName: lastName,
+                    email: email
+                })
+            });
+
+            if (!response.ok) {
+                throw new Error(`Failed to update user. Status code: ${response.status}`);
+            }
+
+            setUser(response);
+            alert('User updated successfully');
+        } catch (error) {
+            alert(`Failed to update user: ${error.message}`);
+        }
+    };
     return (
-        <div>
-            <h1>Profil</h1>
+        <div className="container">
+            <div>
+                <h2>Profil</h2>
+            </div>
+            {user ? (
+                <form onSubmit={handleFormSubmit}>
+                    <div>
+                        <label htmlFor="firstName">Fornavn:</label>
+                        <input type="text" name="firstName" id="firstName" value={firstName} onChange={handleInputChange} />
+                    </div>
+                    <div>
+                        <label htmlFor="lastName">Etternavn:</label>
+                        <input type="text" name="lastName" id="lastName" value={lastName} onChange={handleInputChange} />
+                    </div>
+                    <div>
+                        <label htmlFor="email">Email:</label>
+                        <input type="email" name="email" id="email" value={email} onChange={handleInputChange} />
+                    </div>
+                    <button type="submit">Oppdatter</button>
+                    <button type="application">Se dine søknader</button>
+                    
+                </form>
+            ) : (
+                <p>Loading...</p>
+            )}
         </div>
-    )
-}
+    );
+};
+
 export default withAuth(Profil);
